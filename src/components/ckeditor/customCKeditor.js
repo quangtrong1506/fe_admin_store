80519e168fa2a24b7a0f72943b73a81ecb0c2351
Quang Trong <quangtrong1506@gmail.com>
2023-08-30 11:07:58 +0700

    test

--- src/components/ckeditor/customCKeditor.js
+++ src/components/ckeditor/customCKeditor.js
@@ -1,6 +1,8 @@
-import * as Editor from './ckeditor-custom/build/ckeditor';
+// import  Editor from './ckeditor-custom/build/ckeditor';
+const Editor = require('./ckeditor-custom/build/ckeditor');
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 export default function CustomCKeditor({ data, handleData = () => {} }) {
+    console.log(Editor);
     console.log(Editor.default);
     return (
         <>
